3f1f7b67646e2da7ae6041af3febadb3ba6554b6
Francisc0-byte <pc96ihd@eduvaud.ch>
2025-01-27 15:44:50 +0100

    changement du code

---
+++ Projet POO/Projet POO/Joueur1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_POO
+{
+    class Joueur
+
+    {
+		private string[] _representation;
+
+		public string[] Representation
+		{
+			get { return _representation; }
+			set { _representation = value; }
+		}
+		public Joueur()
+		{
+			//this.Representation = "";
+			
+		}
+		public string affichageJoueur()
+		{
+            // Positionner le curseur tout en bas de la console
+            Console.SetCursorPosition(Console.WindowWidth - 130, consoleHeight - 1);
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 0)
+                    Console.WriteLine("  o  ");
+				if (i == 1) Console.WriteLine(" / ¦ \ ");
+            }
+        }
+    }
+}
---
+++ Projet POO/Projet POO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Taile de la console
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 150;
+
+            // Obtenir la hauteur de la console
+            int consoleHeight = Console.WindowHeight; 
+            int consoleWidth = Console.WindowWidth;
+
+            
+          
+            Console.ReadLine();
+        }
+    }
+}
